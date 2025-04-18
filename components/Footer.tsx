d8f17770d5899ca964a863d4bb8aab6fc9a22c47
gokhanozturk <gokhaanozturk@gmail.com>
2025-04-18 23:00:38 +0300

    Update Footer component to include a link to the source code on GitHub, enhancing user access to project resources.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,16 @@
 export default function Footer() {
   return (
-    <footer className="bg-neutral-100 p-4 text-center text-neutral-500">
+    <footer className="flex items-center justify-center gap-2 bg-neutral-100 p-4 text-center text-neutral-500">
       <p>&copy; 2025 P3 Color Converter</p>
+
+      <a
+        className="underline-offset-2 hover:underline"
+        target="_blank"
+        href="https://github.com/gokh4nozturk/p3-converter"
+        rel="noreferrer"
+      >
+        View the source code on GitHub
+      </a>
     </footer>
   );
 }
